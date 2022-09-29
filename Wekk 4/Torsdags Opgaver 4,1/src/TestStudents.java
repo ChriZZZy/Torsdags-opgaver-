3fe6d8ccb7d666680ae0a4e0cdd432babd013268
Chris_ziegler <60019587+ChriZZZy@users.noreply.github.com>
2022-09-29 13:18:15 +0200

    Torsdag 4

---
+++ Wekk 4/Torsdags Opgaver 4,1/src/TestStudents.java
@@ -0,0 +1,7 @@
+public class TestStudents {
+
+    public static void main(String[] args) {
+        Student s1 = new Student ("chris");
+        Course  c1 = new Course ("programming");
+    }
+}
---
+++ Wekk 4/Torsdags Opgaver 4,4/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> actions = new ArrayList<>();
+        actions.add("Start Game");
+        actions.add("Resume Game");
+        actions.add("Pause Game");
+        actions.add("End Game");
+        Menu menu = new Menu(actions);
+
+    }
+    public static void doAction(int action){
+        
+    }
+}
+
+
+
